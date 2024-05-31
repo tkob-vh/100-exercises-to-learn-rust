8c69d1495c9276a0ceccebbefc8caa1f3a717335
yyx <yuxin.yan.cs@gmail.com>
2024-05-31 09:28:22 -0400

    feat: finish the traits Section

--- exercises/04_traits/10_clone/src/lib.rs
+++ exercises/04_traits/10_clone/src/lib.rs
@@ -1,10 +1,12 @@
 // TODO: add the necessary `Clone` implementations (and invocations)
 //  to get the code to compile.
 
-pub fn summary(ticket: Ticket) -> (Ticket, Summary) {
-    (ticket, ticket.summary())
+pub fn summary(ticket: &Ticket) -> (Ticket, Summary) {
+    (ticket.clone(), ticket.summary())
 }
 
+
+#[derive(Clone)]
 pub struct Ticket {
     pub title: String,
     pub description: String,
@@ -12,14 +14,16 @@ pub struct Ticket {
 }
 
 impl Ticket {
-    pub fn summary(self) -> Summary {
+    pub fn summary(&self) -> Summary {
         Summary {
-            title: self.title,
-            status: self.status,
+            title: self.title.clone(),
+            status: self.status.clone(),
         }
     }
 }
 
+
+#[derive(Clone)]
 pub struct Summary {
     pub title: String,
     pub status: String,
